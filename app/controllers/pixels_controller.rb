eecdeec2e12b4a27db6733fc68323aba63b87ad5
Prian Kuhanandan <priankuhan@gmail.com>
2017-05-27 15:58:45 +0000

    Fix ruby math

--- app/controllers/pixels_controller.rb
+++ app/controllers/pixels_controller.rb
@@ -17,13 +17,14 @@ class PixelsController < ApplicationController
     rounded_lat = round_down(pixel_params["latitude"], Pixel::PRECISION)
     rounded_long = round_down(pixel_params["longitude"], Pixel::PRECISION)
     @pixel = Pixel.find_or_initialize_by(:south => rounded_lat, :west => rounded_long)
-    
+    Rails.logger.debug rounded_lat
+    Rails.logger.debug rounded_long
     @pixel.update_attributes(
       {
-        :south => rounded_lat,
-        :north => rounded_lat + (10 ** -Pixel::PRECISION),
-        :west => rounded_long,
-        :east => rounded_long + (10 ** -Pixel::PRECISION),
+        :south => rounded_lat.round(4),
+        :north => (rounded_lat + (10 ** -Pixel::PRECISION)).round(4),
+        :west => rounded_long.round(4),
+        :east => (rounded_long + (10 ** -Pixel::PRECISION)).round(4),
         :colour => pixel_params["colour"]
       }
     )
@@ -37,7 +38,7 @@ class PixelsController < ApplicationController
   
     def round_down(number, decimal_place = 0)
       factor = 10 ** decimal_place
-      ((number*factor).floor)/factor
+      ((number*factor).floor)/factor.to_f
     end
 
     # Only allow a trusted parameter "white list" through.
